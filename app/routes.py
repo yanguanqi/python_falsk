f257acfbb7ed7bf4e027a3dced61dd546ce2d739
unknown <yanguanqi@aliyun.com>
2019-01-10 17:29:14 +0800

    Flask Web Chapter 1 and 2

---
+++ app/routes.py
@@ -0,0 +1,18 @@
+from app import app
+from flask import render_template
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user={'username':'Miguel'}
+    posts=[
+        {
+            'author':{'username':'John'},
+            'body':'Beautiful day in Portland!'
+        },
+        {
+            'author':{'username':'Susan'},
+            'body':'The Avengers movie wa so cool!'
+        }
+    ]
+    return render_template('index.html',title='Home',user=user,posts=posts)
